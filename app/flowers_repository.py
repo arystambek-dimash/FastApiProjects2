66264a46b44b485c09d141c977ac138c80f6b627
dinmukhammed <arystambekdimash@gmail.com>
2023-07-20 00:01:44 +0600

    start

--- app/flowers_repository.py
+++ app/flowers_repository.py
@@ -11,36 +11,4 @@ class Flower(BaseModel):
 
 
 class FlowersRepository:
-    flowers: list[Flower]
-    carts: list[int]
-
-    def __init__(self):
-        self.flowers = [Flower(name="Roza", count=1, cost=float(5), id=1),
-                        Flower(name="Roza", count=1, cost=float(5), id=2),
-                        Flower(name="Roza", count=1, cost=float(5), id=3)]
-        self.carts = []
-
-    def get_all(self):
-        return self.flowers
-
-    def save(self, flower):
-        if flower.id == 0:
-            flower.id = self.get_next_id()
-        self.flowers.append(flower)
-
-    def get_next_id(self):
-        return len(self.flowers) + 1
-
-    def save_flower_to_cart(self, flower_id):
-        self.carts.append(flower_id)
-
-    def get_all_in_cart(self):
-        return self.carts
-
-    def get_by_id(self, flower_id):
-        cart_flowers = []
-        for i, f in enumerate(self.flowers):
-            if f.id == flower_id:
-                return self.flowers[i]
-
-        return None
+    pass
--- app/users_repository.py
+++ app/users_repository.py
@@ -21,30 +21,4 @@ class UserResponse(BaseModel):
 
 
 class UsersRepository:
-    users: list[UserRequest]
-
-    def __init__(self):
-        self.users = [UserRequest(username="d", email="test@test.com", full_name="D", password="12345678", id=1)]
-
-    def save(self, user):
-        for u in self.users:
-            if user.email == u.email or user.username == u.username:
-                raise HTTPException(status_code=400, detail="The email or username already exists")
-
-        user.id = self.get_next_id()
-        self.users.append(user)
-
-    def get_one_by_id(self, user_id) -> UserRequest:
-        for i, u in enumerate(self.users):
-            if u.id == user_id:
-                return self.users[i]
-        return None
-
-    def get_user_by_username(self, username) -> UserRequest:
-        for i, u in enumerate(self.users):
-            if u.username == username:
-                return self.users[i]
-        return None
-
-    def get_next_id(self):
-        return len(self.users) + 1
+    pass
